ff6673d18fada9949774e8db8b921915cd0367bb
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-07-23 13:47:45 -0400

    ENH: Initial script for visualizing tree and table.

---
+++ PhyloTreeHeatmapVis.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+from  vtk import *
+
+#read in  a tree
+treeReader = vtkNewickTreeReader()
+treeReader.SetFileName('/home/xiaoxiao/work/bin/Visomics/Visomics-build/install/data/anolis.phy')
+treeReader.Update()
+tr = treeReader.GetOutput()
+print(tr.GetNumberOfVertices())
+
+#read in  a table
+tableReader = vtkDelimitedTextReader()
+tableReader.SetFileName('/home/xiaoxiao/work/bin/Visomics/Visomics-build/install/data/anolisDataAppended.csv')
+tableReader.Update()
+table = tableReader.GetOutput()
+
+
+#play with the heatmap vis
+treeHeatmapItem = vtkTreeHeatmapItem()
+treeHeatmapItem.SetTree(tr);
+treeHeatmapItem.SetTable(table);
+
+# setup the window
+view = vtkContextView()
+view.GetRenderer().SetBackground(1,1,1)
+view.GetRenderWindow().SetSize(800,600)
+
+iren = vtkRenderWindowInteractor()
+iren.SetRenderWindow(view.GetRenderWindow())
+
+transformItem = vtkContextTransform()
+transformItem.AddItem(treeHeatmapItem)
+transformItem.SetInteractive(1)
+
+view.GetScene().AddItem(transformItem)
+view.GetRenderWindow().SetMultiSamples(0)
+view.GetRenderWindow().Render()
+
+
+
+
+
